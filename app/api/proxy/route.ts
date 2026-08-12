0424b0fb0142cb5b6eb99591d005d8302e244987
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-08-12 21:59:29 +0530

    Added URL validation in GET request handler

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -2,3 +2,5 @@ import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   const url = req.nextUrl.searchParams.get("url");
+  if (!url) return new Response("url required", { status: 400 });
+  let target: URL;
