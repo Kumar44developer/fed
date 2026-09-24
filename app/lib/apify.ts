eb778312d6dc613b19aa4551350d554d63131f6e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-09-24 19:06:23 +0530

    Added IgProfile interface to apify.ts

--- app/lib/apify.ts
+++ app/lib/apify.ts
@@ -1,3 +1,8 @@
 import { ApifyClient } from "apify-client";
 
 const client = new ApifyClient({ token: process.env.APIFY_TOKEN });
+
+
+export interface IgProfile {
+  username: string;
+  fullName: string;
