267c1f75414acf10e7b5282bcf7532a0854dc118
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-09-24 19:06:34 +0530

    Added profilePicUrl, followersCount, and biography to IgProfile

--- app/lib/apify.ts
+++ app/lib/apify.ts
@@ -6,3 +6,7 @@ const client = new ApifyClient({ token: process.env.APIFY_TOKEN });
 export interface IgProfile {
   username: string;
   fullName: string;
+  profilePicUrl: string;
+  followersCount?: number;
+  biography?: string;
+}
