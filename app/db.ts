9274ada0303f3baed414083d95148a49c83e8c76
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-09-26 10:09:20 +0530

    Added playlists table to database schema

--- app/db.ts
+++ app/db.ts
@@ -34,3 +34,8 @@ db.exec(`
     username TEXT NOT NULL,
     PRIMARY KEY (section_id, username)
   );
+
+  CREATE TABLE IF NOT EXISTS playlists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+  );
